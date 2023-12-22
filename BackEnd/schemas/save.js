97e248c1b19e524e5b7b7cb79e9756334a261a45
3mptySpac3 <thefantasticalproject@gmail.com>
2023-12-21 20:06:18 -0700

    Starting FrontEnd

---
+++ BackEnd/schemas/save.js
@@ -0,0 +1,17 @@
+export default{
+  name: "save",
+  tittle: "Save",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "PostedBy",
+      type: "postedBy"
+    },
+    {
+      name: "UserId",
+      title: "UserId",
+      type: "string"
+    },
+  ]
+}
