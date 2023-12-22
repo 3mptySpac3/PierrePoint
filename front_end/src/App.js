18af9369a1c131ef091e81d99d99c7540ec2976d
3mptySpac3 <thefantasticalproject@gmail.com>
2023-12-22 05:52:58 -0700

    front

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route, useNavigate} from "react-router-dom";
+import Home from './container/Home';
+import Login from "./components/Login";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="Login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+
+    </Routes>
   )
 }
