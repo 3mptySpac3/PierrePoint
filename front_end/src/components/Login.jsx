8f21b3ec0252c1b174ed607c6c81b8bbb098a7c6
3mptySpac3 <thefantasticalproject@gmail.com>
2023-12-27 04:18:52 -0700

    Background Overlay and Sign in with google

--- front_end/src/components/Login.jsx
+++ front_end/src/components/Login.jsx
@@ -2,11 +2,62 @@ import React from 'react';
 import GoogleLogin from "react-google-login";
 import {useNavigate} from "react-router-dom";
 import {FCGoogle} from "react-icons/fc";
+import ppointVideo from "../assets/ppoint.mp4";
+import logo from "../assets/PPPoint.png";
+import { FcGoogle } from 'react-icons/fc';
+
+
 
 
 const Login = () => {
+  const responseGoogle=(response) => {
+    console.log(response);
+
+  }
   return (
-    <div>Login</div>
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+        src={ppointVideo}
+        type="video/mp4"
+        loop
+        control="false"
+        muted
+        autoPlay
+        className='w-full object-cover'
+        />
+
+        <div className='fixed flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay '>
+          <div className='p-5'>
+            <img src={logo} width="150px" alt="logo"/>
+          </div>
+
+          <div className='shadow-2xl'>
+            <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+            render={(renderProps) => (
+              <button
+              type="button"
+              className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+              >
+                <FcGoogle className="mr-4"/> Sign in with Google
+
+              </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+
+
+            
+            />
+
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
