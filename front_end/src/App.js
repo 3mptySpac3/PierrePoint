09238c829fdb01cf5660062ed38b960a15e71188
3mptySpac3 <thefantasticalproject@gmail.com>
2023-12-27 05:49:51 -0700

    runtime error

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Routes, Route, useNavigate} from "react-router-dom";
+import {Routes, Route, } from "react-router-dom";
 import Home from './container/Home';
-import Login from "./components/Login";
+import Login from "./components/login";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="Login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Home />} />
 
     </Routes>
   )
