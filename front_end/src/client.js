373279cc8ac1547e78f28b8d577356d6d3a3c7db
3mptySpac3 <thefantasticalproject@gmail.com>
2023-12-29 04:27:51 -0700

    config front

--- front_end/src/client.js
+++ front_end/src/client.js
@@ -5,10 +5,12 @@ import imageUrlBuilder from '@sanity/image-url'
 export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
-  appVersion: "2023-12-27",
+  appVersion: "2023-12-27", // Specify the API version
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
+  apiVersion: '2023-12-27', // Add the API version here
 });
+
 
 
 const builder = imageUrlBuilder(client);
--- front_end/src/components/Login.jsx
+++ front_end/src/components/Login.jsx
@@ -12,23 +12,31 @@ import {client} from '../client'
 const Login = () => {
   const navigate = useNavigate();
 
-  const responseGoogle=(response) => {
+  const responseGoogle = (response) => {
+    if (!response.profileObj) {
+      console.error('Google login response is missing profileObj:', response);
+      // Handle the error here. For example, you might want to show an error message to the user.
+      return;
+    }
+  
+    // If profileObj is present, proceed with your logic
     localStorage.setItem("user", JSON.stringify(response.profileObj));
-
-    const {name, googleId, imageUrl} = response.profileObj;
-
+  
+    const { name, googleId, imageUrl } = response.profileObj;
+  
     const doc = {
       _id: googleId,
       _type: "user",
       userName: name,
       image: imageUrl,
     }
-
+  
     client.createIfNotExists(doc)
-    .then(() => {
-      navigate("/", {replace: true})
-    })
+      .then(() => {
+        navigate("/", { replace: true });
+      });
   }
+  
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'>
@@ -39,12 +47,12 @@ const Login = () => {
         control="false"
         muted
         autoPlay
-        className='w-full object-cover'
+        className='w-full h-full object-cover'
         />
 
-        <div className='fixed flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay '>
+        <div className='absolute flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay '>
           <div className='p-5'>
-            <img src={logo} width="150px" alt="logo"/>
+            <img src={logo} width="150px" alt="logo" />
           </div>
 
           <div className='shadow-2xl'>
